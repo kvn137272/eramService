a58843dbe2a0f43b2fb2c96268aa4882eb3af424
keivan salmani <kvn137272@gmail.com>
2023-12-29 22:20:19 +0330

    put items in nuxtlink tag

--- components/layouts/theMomentOffs.vue
+++ components/layouts/theMomentOffs.vue
@@ -3,76 +3,84 @@
     class="lg:px-12 flex md:flex-row flex-col gap-3 items-center justify-between"
   >
     <div class="w-80 bg-white p-5 rounded-xl">
-      <div class="bg-blueColor-100 rounded-lg py-5 mb-3">
-        <img
-          src="../../assets/images/iphoneOff.png"
-          alt="iphone off"
-          class="object-fill w-28 mx-auto"
-        />
-      </div>
-      <div class="space-y-3">
-        <p class="text-xs text-greyColor-200">گوشی موبایل</p>
-        <p>آیفون 14 پرو</p>
-        <p class="text-left line-through text-greyColor-200">20,000000</p>
-        <p class="text-left text-blueColor-500 font-extrabold">
-          <span class="text-xs">تومان</span>
-          18,000,000
-        </p>
-      </div>
+      <NuxtLink to="#">
+        <div class="bg-blueColor-100 rounded-lg py-5 mb-3">
+          <img
+            src="../../assets/images/iphoneOff.png"
+            alt="iphone off"
+            class="object-fill w-28 mx-auto"
+          />
+        </div>
+        <div class="space-y-3">
+          <p class="text-xs text-greyColor-200">گوشی موبایل</p>
+          <p>آیفون 14 پرو</p>
+          <p class="text-left line-through text-greyColor-200">20,000000</p>
+          <p class="text-left text-blueColor-500 font-extrabold">
+            <span class="text-xs">تومان</span>
+            18,000,000
+          </p>
+        </div>
+      </NuxtLink>
     </div>
     <div class="w-80 bg-white p-5 rounded-xl">
-      <div class="bg-blueColor-100 rounded-lg py-5 mb-3">
-        <img
-          src="../../assets/images/iphoneOff.png"
-          alt="iphone off"
-          class="object-fill w-28 mx-auto"
-        />
-      </div>
-      <div class="space-y-3">
-        <p class="text-xs text-greyColor-200">گوشی موبایل</p>
-        <p>آیفون 14 پرو</p>
-        <p class="text-left line-through text-greyColor-200">20,000000</p>
-        <p class="text-left text-blueColor-500 font-extrabold">
-          <span class="text-xs">تومان</span>
-          18,000,000
-        </p>
-      </div>
+      <NuxtLink to="#">
+        <div class="bg-blueColor-100 rounded-lg py-5 mb-3">
+          <img
+            src="../../assets/images/iphoneOff.png"
+            alt="iphone off"
+            class="object-fill w-28 mx-auto"
+          />
+        </div>
+        <div class="space-y-3">
+          <p class="text-xs text-greyColor-200">گوشی موبایل</p>
+          <p>آیفون 14 پرو</p>
+          <p class="text-left line-through text-greyColor-200">20,000000</p>
+          <p class="text-left text-blueColor-500 font-extrabold">
+            <span class="text-xs">تومان</span>
+            18,000,000
+          </p>
+        </div>
+      </NuxtLink>
     </div>
     <div class="w-80 bg-white p-5 rounded-xl">
-      <div class="bg-blueColor-100 rounded-lg py-5 mb-3">
-        <img
-          src="../../assets/images/iphoneOff.png"
-          alt="iphone off"
-          class="object-fill w-28 mx-auto"
-        />
-      </div>
-      <div class="space-y-3">
-        <p class="text-xs text-greyColor-200">گوشی موبایل</p>
-        <p>آیفون 14 پرو</p>
-        <p class="text-left line-through text-greyColor-200">20,000000</p>
-        <p class="text-left text-blueColor-500 font-extrabold">
-          <span class="text-xs">تومان</span>
-          18,000,000
-        </p>
-      </div>
+      <NuxtLink to="#">
+        <div class="bg-blueColor-100 rounded-lg py-5 mb-3">
+          <img
+            src="../../assets/images/iphoneOff.png"
+            alt="iphone off"
+            class="object-fill w-28 mx-auto"
+          />
+        </div>
+        <div class="space-y-3">
+          <p class="text-xs text-greyColor-200">گوشی موبایل</p>
+          <p>آیفون 14 پرو</p>
+          <p class="text-left line-through text-greyColor-200">20,000000</p>
+          <p class="text-left text-blueColor-500 font-extrabold">
+            <span class="text-xs">تومان</span>
+            18,000,000
+          </p>
+        </div>
+      </NuxtLink>
     </div>
     <div class="w-80 bg-white p-5 rounded-xl">
-      <div class="bg-blueColor-100 rounded-lg py-5 mb-3">
-        <img
-          src="../../assets/images/iphoneOff.png"
-          alt="iphone off"
-          class="object-fill w-28 mx-auto"
-        />
-      </div>
-      <div class="space-y-3">
-        <p class="text-xs text-greyColor-200">گوشی موبایل</p>
-        <p>آیفون 14 پرو</p>
-        <p class="text-left line-through text-greyColor-200">20,000000</p>
-        <p class="text-left text-blueColor-500 font-extrabold">
-          <span class="text-xs">تومان</span>
-          18,000,000
-        </p>
-      </div>
+      <NuxtLink to="#">
+        <div class="bg-blueColor-100 rounded-lg py-5 mb-3">
+          <img
+            src="../../assets/images/iphoneOff.png"
+            alt="iphone off"
+            class="object-fill w-28 mx-auto"
+          />
+        </div>
+        <div class="space-y-3">
+          <p class="text-xs text-greyColor-200">گوشی موبایل</p>
+          <p>آیفون 14 پرو</p>
+          <p class="text-left line-through text-greyColor-200">20,000000</p>
+          <p class="text-left text-blueColor-500 font-extrabold">
+            <span class="text-xs">تومان</span>
+            18,000,000
+          </p>
+        </div>
+      </NuxtLink>
     </div>
   </section>
 </template>
