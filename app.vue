fbbce1774f31bcda34dce92741fcc48d0ee37282
keivan salmani <kvn137272@gmail.com>
2023-12-30 17:20:33 +0330

    filter branch completed

--- app.vue
+++ app.vue
@@ -7,6 +7,7 @@
     <the-brands/>
     <the-accessories/>
     <the-moment-offs/>
+    <the-filter-products/>
     <the-counseling-form/>
     <the-footer/>
   </div>
---
+++ components/layouts/theFilterProducts.vue
@@ -0,0 +1,130 @@
+<template>
+  <section class="mx-12 my-24 flex flex-col space-y-16 ">
+    <div class="flex lg:flex-row flex-col lg:space-y-0 space-y-10">
+      <!-- filter title section -->
+      <div class="lg:ml-16">
+        <p class="text-lg font-semibold md:text-right text-center">
+          گوشی با هر بودجه ای
+        </p>
+      </div>
+
+      <!-- filter items section -->
+      <div
+        class="flex md:flex-row flex-col items-center md:space-y-0 space-y-6 lg:gap-12 gap-4"
+      >
+        <div>
+          <NuxtLink
+            to="#"
+            class="py-3 hover:bg-greyColor-300 group px-8 border border-greyColor-100 font-semibold text-sm relative"
+          >
+            زیر 5 میلیون
+            <span
+              class="bg-black invisible group-hover:visible transition-all duration-75 w-16 h-[3px] absolute left-0 right-0 -bottom-0.5 mx-auto"
+              >&nbsp;</span
+            >
+          </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink
+            to="#"
+            class="py-3 hover:bg-greyColor-300 group px-8 border border-greyColor-100 font-semibold text-sm relative"
+          >
+            زیر 5 میلیون
+            <span
+              class="bg-black invisible group-hover:visible transition-all duration-75 w-16 h-[3px] absolute left-0 right-0 -bottom-0.5 mx-auto"
+              >&nbsp;</span
+            >
+          </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink
+            to="#"
+            class="py-3 hover:bg-greyColor-300 group px-8 border border-greyColor-100 font-semibold text-sm relative"
+          >
+            زیر 5 میلیون
+            <span
+              class="bg-black invisible group-hover:visible transition-all duration-75 w-16 h-[3px] absolute left-0 right-0 -bottom-0.5 mx-auto"
+              >&nbsp;</span
+            >
+          </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink
+            to="#"
+            class="py-3 hover:bg-greyColor-300 group px-8 border border-greyColor-100 font-semibold text-sm relative"
+          >
+            زیر 5 میلیون
+            <span
+              class="bg-black invisible group-hover:visible transition-all duration-75 w-16 h-[3px] absolute left-0 right-0 -bottom-0.5 mx-auto"
+              >&nbsp;</span
+            >
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+   
+    <div class="flex lg:flex-row flex-col lg:space-y-0 space-y-10">
+      <!-- filter title section -->
+      <div class="lg:ml-16">
+        <p class="text-lg font-semibold md:text-right text-center">
+          گوشی با هر بودجه ای
+        </p>
+      </div>
+
+      <!-- filter items section -->
+      <div
+        class="flex md:flex-row flex-col items-center md:space-y-0 space-y-6 lg:gap-12 gap-4"
+      >
+        <div>
+          <NuxtLink
+            to="#"
+            class="py-3 hover:bg-greyColor-300 group px-8 border border-greyColor-100 font-semibold text-sm relative"
+          >
+            زیر 5 میلیون
+            <span
+              class="bg-black invisible group-hover:visible transition-all duration-75 w-16 h-[3px] absolute left-0 right-0 -bottom-0.5 mx-auto"
+              >&nbsp;</span
+            >
+          </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink
+            to="#"
+            class="py-3 hover:bg-greyColor-300 group px-8 border border-greyColor-100 font-semibold text-sm relative"
+          >
+            زیر 5 میلیون
+            <span
+              class="bg-black invisible group-hover:visible transition-all duration-75 w-16 h-[3px] absolute left-0 right-0 -bottom-0.5 mx-auto"
+              >&nbsp;</span
+            >
+          </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink
+            to="#"
+            class="py-3 hover:bg-greyColor-300 group px-8 border border-greyColor-100 font-semibold text-sm relative"
+          >
+            زیر 5 میلیون
+            <span
+              class="bg-black invisible group-hover:visible transition-all duration-75 w-16 h-[3px] absolute left-0 right-0 -bottom-0.5 mx-auto"
+              >&nbsp;</span
+            >
+          </NuxtLink>
+        </div>
+        <div>
+          <NuxtLink
+            to="#"
+            class="py-3 hover:bg-greyColor-300 group px-8 border border-greyColor-100 font-semibold text-sm relative"
+          >
+            زیر 5 میلیون
+            <span
+              class="bg-black invisible group-hover:visible transition-all duration-75 w-16 h-[3px] absolute left-0 right-0 -bottom-0.5 mx-auto"
+              >&nbsp;</span
+            >
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+   
+  </section>
+</template>
